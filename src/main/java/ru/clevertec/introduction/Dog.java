f7e29c554b060c5d02a4d181dc7452780ab3ca84
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-15 15:40:29 +0300

    6. Dependency Injection с помощью конструктора

--- src/main/java/ru/clevertec/introduction/Dog.java
+++ src/main/java/ru/clevertec/introduction/Dog.java
@@ -1,6 +1,10 @@
 package ru.clevertec.introduction;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("gav-gav");
