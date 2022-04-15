d872baf4efdeee6c8b8afd05a46a86b54cba218f
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-15 15:54:57 +0300

    7. Dependency Injection с помощью сеттера

--- src/main/java/ru/clevertec/introduction/Ex3.java
+++ src/main/java/ru/clevertec/introduction/Ex3.java
@@ -6,8 +6,6 @@ public class Ex3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-//        Pet pet = new Cat();
-//        Pet pet = context.getBean("myPet", Pet.class);
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
         context.close();
--- src/main/java/ru/clevertec/introduction/Person.java
+++ src/main/java/ru/clevertec/introduction/Person.java
@@ -3,8 +3,17 @@ package ru.clevertec.introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
