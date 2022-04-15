88e274cd1bf5e3e0cec15fe25109fac0a00281e8
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-15 16:11:34 +0300

    8. Внедрение строк и других значений

--- src/main/java/ru/clevertec/introduction/Person.java
+++ src/main/java/ru/clevertec/introduction/Person.java
@@ -2,6 +2,8 @@ package ru.clevertec.introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
 //    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
@@ -15,6 +17,24 @@ public class Person {
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
     }
 
     public void callYourPet() {
