26e3a0ffc17993310ffb60b6704de5cc0a247682
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-15 15:18:46 +0300

    5. Dependency Injection. Знакомство

---
+++ src/main/java/ru/clevertec/introduction/Ex3.java
@@ -0,0 +1,15 @@
+package ru.clevertec.introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+//        Pet pet = new Cat();
+        Pet pet = context.getBean("myPet", Pet.class);
+        Person person = new Person(pet);
+        person.callYourPet();
+        context.close();
+    }
+}
