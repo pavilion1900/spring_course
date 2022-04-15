c27fad0c816c4d513231e6d102a271c1bcf3c944
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-15 15:18:46 +0300

    5. Dependency Injection. Знакомство

---
+++ src/main/java/ru/clevertec/introduction/Person.java
@@ -0,0 +1,14 @@
+package ru.clevertec.introduction;
+
+public class Person {
+    private Pet pet;
+
+    public Person(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void callYourPet() {
+        System.out.println("Hello my pet");
+        pet.say();
+    }
+}
