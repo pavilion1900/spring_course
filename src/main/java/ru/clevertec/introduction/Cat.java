c846d0467b3ecb6a81bfcad01e1d428b1610d6f8
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-18 15:21:32 +0300

    14. Аннотация @Autowired для конструктора

--- src/main/java/ru/clevertec/introduction/Cat.java
+++ src/main/java/ru/clevertec/introduction/Cat.java
@@ -2,7 +2,7 @@ package ru.clevertec.introduction;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("catBean")
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
--- src/main/java/ru/clevertec/introduction/Ex6.java
+++ src/main/java/ru/clevertec/introduction/Ex6.java
@@ -6,8 +6,10 @@ public class Ex6 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext3.xml");
-        Cat cat = context.getBean("cat", Cat.class);
-        cat.say();
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+//        Cat cat = context.getBean("cat", Cat.class);
+//        cat.say();
         context.close();
     }
 }
