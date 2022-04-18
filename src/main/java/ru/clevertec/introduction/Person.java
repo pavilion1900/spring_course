91336a7c13725c53f372b1442e05aeedde12b7fe
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-18 15:32:43 +0300

    16. Аннотация @Autowired для поля

--- src/main/java/ru/clevertec/introduction/Person.java
+++ src/main/java/ru/clevertec/introduction/Person.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
+    @Autowired
     private Pet pet;
     private String surname;
     private int age;
@@ -19,7 +20,7 @@ public class Person {
         System.out.println("Person bean is created");
     }
 
-    @Autowired
+    //    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
