9d17e5e6d27146c565518fa54821d7fe8d022977
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-18 19:00:13 +0300

    20. Конфигурация с помощью Java code. Способ 1

---
+++ src/main/java/ru/clevertec/introduction/MyConfig.java
@@ -0,0 +1,9 @@
+package ru.clevertec.introduction;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.clevertec.introduction")
+public class MyConfig {
+}
