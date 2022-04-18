0494fbd3038bd93e9431a0b5fff0e89c97f08d45
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-18 19:35:40 +0300

    22. Аннотация @PropertySource

--- src/main/java/ru/clevertec/introduction/Ex8.java
+++ src/main/java/ru/clevertec/introduction/Ex8.java
@@ -9,9 +9,11 @@ public class Ex8 {
 //        Pet firstCat = context.getBean("catBean", Pet.class);
 //        Pet secondCat = context.getBean("catBean", Pet.class);
         Person person = context.getBean("personBean", Person.class);
-        Person person2 = context.getBean("personBean", Person.class);
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+//        Person person2 = context.getBean("personBean", Person.class);
 //        person.callYourPet();
-        System.out.println(person == person2);
+//        System.out.println(person == person2);
         context.close();
     }
 }
