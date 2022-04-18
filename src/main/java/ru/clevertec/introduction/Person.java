2c18a8c781c27886a8bb28378dcfca6c749ce12d
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-18 15:27:42 +0300

    15. Аннотация @Autowired для сеттера

--- src/main/java/ru/clevertec/introduction/Person.java
+++ src/main/java/ru/clevertec/introduction/Person.java
@@ -9,16 +9,17 @@ public class Person {
     private String surname;
     private int age;
 
-    @Autowired
-    public Person(Pet pet) {
-        System.out.println("Person bean is created");
-        this.pet = pet;
-    }
-
-//    public Person() {
+//    @Autowired
+//    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
+//        this.pet = pet;
 //    }
 
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
+    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
