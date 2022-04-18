8052fec2e052e0a4a283268c862ccbc24fa57bcd
Sergey Korolenko <pavilion1900@gmail.com>
2022-04-18 19:24:49 +0300

    21. Конфигурация с помощью Java code. Способ 2

--- src/main/java/ru/clevertec/introduction/Cat.java
+++ src/main/java/ru/clevertec/introduction/Cat.java
@@ -1,14 +1,16 @@
 package ru.clevertec.introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component("catBean")
+//@Component("catBean")
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
     }
 
     @Override
+    @Scope("singleton")
     public void say() {
         System.out.println("may-may");
     }
