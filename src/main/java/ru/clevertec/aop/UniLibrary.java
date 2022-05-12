39475109230988a588fbabc836ae37008f509cf9
Sergey Korolenko <pavilion1900@gmail.com>
2022-05-12 20:53:53 +0300

    6. Комбинирование Pointcut

--- src/main/java/ru/clevertec/aop/UniLibrary.java
+++ src/main/java/ru/clevertec/aop/UniLibrary.java
@@ -8,11 +8,23 @@ public class UniLibrary extends AbstractLibrary {
         System.out.println("We take a book from UniLibrary");
     }
 
-//    public void returnBook() {
-//        System.out.println("We return book to UniLibrary");
-//    }
+    public void returnBook() {
+        System.out.println("We return a book to UniLibrary");
+    }
 
     public void getMagazine() {
         System.out.println("We take a magazine from UniLibrary");
+    }
+
+    public void returnMagazine() {
+        System.out.println("We return a magazine to UniLibrary");
+    }
+
+    public void addBook() {
+        System.out.println("We add a book to UniLibrary");
+    }
+
+    public void addMagazine() {
+        System.out.println("We add a magazine to UniLibrary");
     }
 }
