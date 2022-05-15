a16762bf5f34823d0d102b2f813773a90cbd1282
Sergey Korolenko <pavilion1900@gmail.com>
2022-05-15 17:18:17 +0300

    8. Join Point

--- src/main/java/ru/clevertec/aop/Book.java
+++ src/main/java/ru/clevertec/aop/Book.java
@@ -7,8 +7,20 @@ import org.springframework.stereotype.Component;
 public class Book {
     @Value("Harry Potter")
     private String name;
+    @Value(("Rowling"))
+    private String author;
+    @Value("1997")
+    private int yearOfPublication;
 
     public String getName() {
         return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
     }
 }
--- src/main/java/ru/clevertec/aop/UniLibrary.java
+++ src/main/java/ru/clevertec/aop/UniLibrary.java
@@ -6,25 +6,31 @@ import org.springframework.stereotype.Component;
 public class UniLibrary extends AbstractLibrary {
     public void getBook() {
         System.out.println("We take a book from UniLibrary");
+        System.out.println("------------------------------------");
     }
 
     public void returnBook() {
         System.out.println("We return a book to UniLibrary");
+        System.out.println("------------------------------------");
     }
 
     public void getMagazine() {
         System.out.println("We take a magazine from UniLibrary");
+        System.out.println("------------------------------------");
     }
 
     public void returnMagazine() {
         System.out.println("We return a magazine to UniLibrary");
+        System.out.println("------------------------------------");
     }
 
-    public void addBook() {
+    public void addBook(String personName, Book book) {
         System.out.println("We add a book to UniLibrary");
+        System.out.println("------------------------------------");
     }
 
     public void addMagazine() {
         System.out.println("We add a magazine to UniLibrary");
+        System.out.println("------------------------------------");
     }
 }
