d613884322602b6a5dcf1f892b28ae0cfedd5035
Korolenko Sergey <pavilion1900@gmail.com>
2022-06-14 00:06:06 +0300

    7. Получение Java объектов из БД

--- src/main/java/ru/clevertec/hibernate/Ex2.java
+++ src/main/java/ru/clevertec/hibernate/Ex2.java
@@ -5,19 +5,24 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.clevertec.hibernate.entity.Employee;
 
-public class Ex1 {
+public class Ex2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
         try {
-            Employee emp1 = new Employee("Alex", "Ivanov", "IT", 600);
+            Employee emp1 = new Employee("Oleg", "Sidorov", "HR", 700);
             Session session = factory.getCurrentSession();
             session.beginTransaction();
             session.save(emp1);
+//            session.getTransaction().commit();
+            int myId = emp1.getId();
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+            Employee emp2 = session.get(Employee.class, myId);
             session.getTransaction().commit();
-            System.out.println(emp1);
+            System.out.println(emp2);
         } finally {
             factory.close();
         }
