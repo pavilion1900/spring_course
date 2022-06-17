d260a39f8a736345498be2a76cc52cf8a7d96e67
Korolenko Sergey <pavilion1900@gmail.com>
2022-06-17 16:58:19 +0300

    17. Отношение Многие-ко-Многим. Часть 1

---
+++ src/main/java/ru/clevertec/hibernate2to2/Ex1.java
@@ -0,0 +1,60 @@
+package ru.clevertec.hibernate2to2;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.clevertec.hibernate2to2.entity.Child;
+import ru.clevertec.hibernate2to2.entity.Section;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Child.class)
+                .addAnnotatedClass(Section.class)
+                .buildSessionFactory();
+        Session session = null;
+        try {
+//            Section section1 = new Section("football");
+//            Child child1 = new Child("Ivan", 5);
+//            Child child2 = new Child("Nick", 7);
+//            Child child3 = new Child("Oleg", 6);
+//            section1.addChildToSection(child1);
+//            section1.addChildToSection(child2);
+//            section1.addChildToSection(child3);
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            session.save(section1);
+//            session.getTransaction().commit();
+
+//            Section section1 = new Section("basketball");
+//            Section section2 = new Section("chess");
+//            Section section3 = new Section("math");
+//            Child child1 = new Child("Timyr", 10);
+//            child1.addSectionToChild(section1);
+//            child1.addSectionToChild(section2);
+//            child1.addSectionToChild(section3);
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            session.save(child1);
+//            session.getTransaction().commit();
+
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Section section = session.get(Section.class, 1);
+//            System.out.println(section);
+//            section.getChildList().forEach(System.out::println);
+//            session.getTransaction().commit();
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Child child = session.get(Child.class, 4);
+            System.out.println(child);
+            child.getSections().forEach(System.out::println);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
